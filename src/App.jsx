22f77d8823407ca9b14abef08342bfacf0337eb2
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-06-20 23:24:51 +0800

    chore: update routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import NotFound from './pages/NotFound';
 import Game from './pages/Game';
 import Home from './pages/Home';
 import End from './pages/End';
+import About from './pages/About';
+import Ranking from './pages/Ranking';
 
 function App() {
   const [player, setPlayer] = useState({ name: 'Unknown', score: 0 });
@@ -19,13 +21,15 @@ function App() {
 
   return (
     <Router>
-      <div className="mx-auto flex h-screen w-screen max-w-2xl flex-col items-center justify-start p-5 md:gap-5 md:p-10">
+      <div className="mx-auto flex h-screen min-h-screen w-screen max-w-2xl flex-col items-center justify-center p-5 md:gap-5 md:p-10">
         <Header />
 
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/game" element={<Game onEnd={handleQuizEnd} />} />
           <Route path="/end" element={<End score={player.score} />} />
+          <Route path="/ranking" element={<Ranking />} />
+          <Route path="/about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
