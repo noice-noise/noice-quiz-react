18923fb66fa5fb3099c7208c18c9839f1468b283
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-06-20 23:23:18 +0800

    feat: update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { HiOutlineEmojiHappy } from 'react-icons/hi';
 import Nav from '../components/Nav';
 import NavItem from '../components/NavItem';
 
 export default function Home() {
   return (
     <Nav>
+      <div className="py-5">
+        <HiOutlineEmojiHappy className="h-10 w-10 animate-bounce hover:animate-spin" />
+      </div>
       <NavItem href="/game">Play</NavItem>
-      <NavItem href="/ranking">Ranking</NavItem>
+      <NavItem href="/ranking">Rankings</NavItem>
       <NavItem href="/about">About</NavItem>
     </Nav>
   );
